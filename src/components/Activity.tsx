a41e52bc168f7ae6fe7ee04a59b262a5c8fe1ae3
nogo <0xnogo@proton.me>
2023-11-13 08:58:21 +0400

    Increase title size

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -1,5 +1,5 @@
 
-import { Badge, Card, CardBody, CardFooter, CardHeader, Divider } from '@chakra-ui/react';
+import { Badge, Card, CardBody, CardFooter, CardHeader, Divider, Heading } from '@chakra-ui/react';
 import React from 'react';
 
 interface ActivityProps {
@@ -12,7 +12,7 @@ interface ActivityProps {
 const Activity: React.FC<ActivityProps> = ({ title, description, location, type }) => {
   return (
     <Card maxW='100%'>
-      <CardHeader>{title} - {location}</CardHeader>
+      <CardHeader><Heading size='sm' fontSize='20px'>{title} - {location}</Heading></CardHeader>
       <CardBody>{description}</CardBody>
       <Divider />
       <CardFooter>
